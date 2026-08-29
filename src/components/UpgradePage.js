ccf1db4bde6511c39889e843939b073ac41a52d1
olaniyanoluwaferanmiolamide20-cpu <olaniyanoluwaferanmiolamide20@gmail.com>
2026-08-29 16:52:53 +0100

    Update UpgradePage.js

--- src/components/UpgradePage.js
+++ src/components/UpgradePage.js
@@ -122,7 +122,7 @@ const UpgradePage = () => {
 
   const finalizeUpgrade = async () => {
     await updateProgressTracking('upgrade-page', true);
-    window.open('https://wa.me/18607976957', '_blank');
+    window.open('https://wa.me/14722684641', '_blank');
     setTimeout(() => navigate('/dashboard'), 1500);
   };
 
